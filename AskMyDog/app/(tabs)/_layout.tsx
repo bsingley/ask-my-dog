6381422233e285c302ab8f32ab342b5f7dffc44e
Beth Singley <beth.singley@gmail.com>
2026-03-31 12:38:31 -0400

    style #6

--- AskMyDog/app/(tabs)/_layout.tsx
+++ AskMyDog/app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
           borderTopColor: '#C4A882', 
           borderTopWidth: 0.5,
           height: Platform.OS === 'android' ? 100 : 70,
-          paddingBottom: Platform.OS === 'android' ? 45 : 10,
+          paddingBottom: Platform.OS === 'android' ? 65 : 10,
           paddingTop: 8,
         },
         tabBarLabelStyle: { fontSize: 14, fontWeight: '600', marginTop: 2 },
